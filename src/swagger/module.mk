5af8e3de2f0fe673d0e1a7ad19b5f6f9536dd543
Timmons C. Player <timmons.player@spirent.com>
2018-09-27 09:23:29 -0400

    swagger: condense mk files
    The swagger model generation build step deletes the model directory,
    so let's not put any build critical files in there.

--- src/swagger/module.mk
+++ src/swagger/module.mk
@@ -1,6 +1,33 @@
 SWAGGER_DEPENDS += json
 SWAGGER_INCLUDES += v1/model
 
-include $(SWAGGER_SRC_DIR)/v1/model/module.mk
+MODEL_SOURCES := \
+	BulkCreateInterfacesRequest.cpp \
+	BulkCreateInterfacesResponse.cpp \
+	BulkDeleteInterfacesRequest.cpp \
+	Interface_config.cpp \
+	Interface.cpp \
+	InterfaceProtocolConfig.cpp \
+	InterfaceProtocolConfig_eth.cpp \
+	InterfaceProtocolConfig_ipv4.cpp \
+	InterfaceProtocolConfig_ipv4_dhcp.cpp \
+	InterfaceProtocolConfig_ipv4_static.cpp \
+	InterfaceStats.cpp \
+	ModelBase.cpp \
+	PortConfig_bond.cpp \
+	PortConfig.cpp \
+	PortConfig_dpdk.cpp \
+	Port.cpp \
+	PortDriver_bond.cpp \
+	PortDriver.cpp \
+	PortDriver_dpdk.cpp \
+	PortDriver_host.cpp \
+	PortStats.cpp \
+	PortStatus.cpp \
+	Stack.cpp \
+	StackElementStats.cpp \
+	StackMemoryStats.cpp \
+	StackProtocolStats.cpp \
+	StackStats.cpp
 
 SWAGGER_SOURCES += $(addprefix v1/model/,$(MODEL_SOURCES))
