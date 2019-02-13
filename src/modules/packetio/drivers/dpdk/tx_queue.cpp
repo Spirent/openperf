8e9dfbd0a3dc3a4952c8678f683a0a1dfae5dfef
Timmons C. Player <timmons.player@spirent.com>
2019-02-13 15:16:44 +0000

    modules/packetio: simplify tx queue
    In an attempt to send multiple packets per burst, the tx queue
    implementation used timerfd's to wake up workers after a short delay
    when the stack enqueued packets for transmit.
    
    With the addition of GSO, this delay is no longer necessary.  So replace
    the timerfd with an eventfd.  This makes the notification process much
    simpler and removes latency from the tx path.

--- src/modules/packetio/drivers/dpdk/tx_queue.cpp
+++ src/modules/packetio/drivers/dpdk/tx_queue.cpp
@@ -1,5 +1,4 @@
 #include <cerrno>
-#include <chrono>
 #include <memory>
 #include <string>
 #include <time.h>
@@ -7,7 +6,7 @@
 #include <vector>
 
 #include <sys/epoll.h>
-#include <sys/timerfd.h>
+#include <sys/eventfd.h>
 
 #include "packetio/drivers/dpdk/model/physical_port.h"
 #include "packetio/drivers/dpdk/tx_queue.h"
@@ -17,37 +16,18 @@ namespace icp {
 namespace packetio {
 namespace dpdk {
 
-using namespace std::chrono_literals;
-static constexpr auto tcp_transmit_delay = 50us;
-
 static std::string get_ring_name(uint16_t port_id, uint16_t queue_id)
 {
     return (std::string("tx_ring_") + std::to_string(port_id)
             + "_" + std::to_string(queue_id));
 }
 
-/* Read the fd to clear it */
-static void tx_queue_epoll_callback(int fd,
-                                    void* arg __attribute__((unused)))
-{
-    uint64_t counter = 0;
-    while (read(fd, &counter, sizeof(counter)) < 0) {
-        if (errno == EINTR) continue;
-        if (errno != EWOULDBLOCK && errno != EAGAIN) {
-            ICP_LOG(ICP_LOG_ERROR, "Error reading from timer fd %d: %s\n",
-                    fd, strerror(errno));
-        }
-        break;
-    }
-}
-
 tx_queue::tx_queue(uint16_t port_id, uint16_t queue_id)
     : m_port(port_id)
     , m_queue(queue_id)
-    , m_fd(timerfd_create(CLOCK_MONOTONIC, TFD_NONBLOCK))
+    , m_fd(eventfd(0, EFD_NONBLOCK))
     , m_enabled(false)
     , m_armed(false)
-    , m_flushed(false)
     , m_ring(rte_ring_create(get_ring_name(port_id, queue_id).c_str(),
                              tx_queue_size,
                              SOCKET_ID_ANY,
@@ -82,7 +62,7 @@ bool tx_queue::add(int poll_fd, void* data)
         .epdata = {
             .event = EPOLLIN | EPOLLET,
             .data = data,
-            .cb_fun = tx_queue_epoll_callback,
+            .cb_fun = nullptr,
             .cb_arg = nullptr
         }
     };
@@ -120,18 +100,14 @@ bool tx_queue::enable()
 bool tx_queue::disable()
 {
     if (m_armed.test_and_set(std::memory_order_acquire)) {
-        auto disarm = itimerspec{
-            .it_interval = {0, 0},
-            .it_value = {0, 0}
-        };
-        if (timerfd_settime(m_fd, 0, &disarm, nullptr) == -1) {
-            ICP_LOG(ICP_LOG_ERROR, "Could not disarm timerfd for tx queue on %u:%u: %s\n",
-                    port_id(), queue_id(), strerror(errno));
+        uint64_t counter = 0;
+        if (eventfd_read(m_fd, &counter) == -1) {
+            ICP_LOG(ICP_LOG_ERROR, "Could not clear fd %d for tx queue %u:%u: %s\n",
+                    m_fd, port_id(), queue_id(), strerror(errno));
             return (false);
         }
     }
     m_enabled.store(false, std::memory_order_release);
-    m_flushed.clear(std::memory_order_release);
     m_armed.clear(std::memory_order_release);
     return (true);
 }
@@ -148,35 +124,11 @@ uint16_t tx_queue::enqueue(rte_mbuf* const mbufs[], uint16_t nb_mbufs)
 
     if (m_enabled.load(std::memory_order_acquire)) {
         if (!m_armed.test_and_set(std::memory_order_acquire)) {
-            /*
-             * If we haven't armed the timer, do so.  We need to let the tx worker
-             * know it has packets to send.  The small delay is to hopefully allow
-             * the worker to send a burst of packets.
-             */
-            auto ns_delay = std::chrono::duration_cast<std::chrono::nanoseconds>(tcp_transmit_delay);
-            auto alarm = itimerspec{
-                .it_interval = { 0, 0 },
-                .it_value    = { 0, ns_delay.count() }
-            };
-            if (timerfd_settime(m_fd, 0, &alarm, nullptr) == -1) {
-                ICP_LOG(ICP_LOG_ERROR, "Could not arm timerfd for tx queue on %u:%u: %s\n",
-                        port_id(), queue_id(), strerror(errno));
-                m_armed.clear(std::memory_order_release);  /* not armed */
-            }
-        } else if (rte_ring_count(ring()) > (tx_queue_size / 4)
-            && !m_flushed.test_and_set(std::memory_order_acquire)) {
-            /*
-             * If the ring is filling up and we haven't setup a flush timer, do so.
-             * We need to get this buffer transmitted before it fills up.
-             */
-            auto flush = itimerspec{
-                .it_interval = { 0, 0},
-                .it_value = { 0, 1}
-            };
-            if (timerfd_settime(m_fd, 0, &flush, nullptr) == -1) {
-                ICP_LOG(ICP_LOG_ERROR, "Could not flush timerfd for tx queue on %u:%u: %s\n",
-                        port_id(), queue_id(), strerror(errno));
-                m_flushed.clear(std::memory_order_release);
+            /* If we haven't armed the eventfd, do so now. */
+            if (eventfd_write(m_fd, 1U) == -1) {
+                ICP_LOG(ICP_LOG_ERROR, "Could not write notification to fd %d for tx queue %u:%u: %s\n",
+                        m_fd, port_id(), queue_id(), strerror(errno));
+                m_armed.clear(std::memory_order_release); /* not armed */
             }
         }
     }
