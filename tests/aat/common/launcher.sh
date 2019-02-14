cba977f255d45aa228b245f233042a12f3bda626
Dan Morton <daniel.morton@spirent.com>
2019-02-14 09:19:59 -1000

    Updates to AATs and framework (#26)
    *Add support for capabilities on inception binary. This will
     allow non-root users to run inception.
    *Add a launcher script to AAT framework to try running as current user if required
     capabilities are found. It will fall back on sudo if not.
    *Update AAT framework to automatically remove stale server Unix
     domain socket and shared memory segment if found on AAT launch.
     This is a temporary workaround for reliability.
    
    Closes #25

---
+++ tests/aat/common/launcher.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+check_command="/sbin/setcap -q -v CAP_IPC_LOCK,CAP_NET_RAW,CAP_SYS_RAWIO,CAP_IPC_OWNER,CAP_NET_BIND_SERVICE,CAP_SETUID,CAP_SYS_ADMIN,CAP_FOWNER,CAP_DAC_OVERRIDE=epi $1"
+
+if $check_command; then
+    exec "$@"
+else
+    exec /usr/bin/sudo "$@"
+fi
