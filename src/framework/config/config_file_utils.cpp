e276a1e91fbf4ea41c39b14ff0fc5ffd4fe14d5a
Daniel Morton <daniel.morton@spirent.com>
2019-04-18 10:04:39 -1000

    Switch from const std::string to std::string_view
    Make better use of C++17 like the cool kids do :).

--- src/framework/config/config_file_utils.cpp
+++ src/framework/config/config_file_utils.cpp
@@ -4,32 +4,32 @@
 
 namespace icp::config::file {
 
-bool icp_config_is_number(const std::string &value)
+bool icp_config_is_number(const std::string_view value)
 {
     if (value.empty()) { return false; }
 
     char *endptr;
 
     // Is it an integer?
-    strtoll(value.c_str(), &endptr, 0);
-    if ((endptr - value.c_str()) == value.length()) { return true; }
+    strtoll(value.data(), &endptr, 0);
+    if ((endptr - value.data()) == value.length()) { return true; }
 
     // How about a double?
-    strtod(value.c_str(), &endptr);
-    if ((endptr - value.c_str()) == value.length()) { return true; }
+    strtod(value.data(), &endptr);
+    if ((endptr - value.data()) == value.length()) { return true; }
 
     return false;
 }
 
-std::tuple<std::string, std::string> icp_config_split_path_id(const std::string &path_id)
+std::tuple<std::string_view, std::string_view> icp_config_split_path_id(std::string_view path_id)
 {
     size_t last_slash_location = path_id.find_last_of('/');
-    if (last_slash_location == std::string::npos) {
+    if (last_slash_location == std::string_view::npos) {
         // No slash. Not a valid /path/id string.
-        return std::tuple(std::string(), std::string());
+        return std::tuple(std::string_view(), std::string_view());
     }
 
-    if (last_slash_location == 0) { return std::tuple(path_id, std::string()); }
+    if (last_slash_location == 0) { return std::tuple(path_id, std::string_view()); }
 
     return std::tuple(path_id.substr(0, last_slash_location),
                       path_id.substr(last_slash_location + 1));
--- src/framework/config/config_file_utils.h
+++ src/framework/config/config_file_utils.h
@@ -8,13 +8,14 @@ namespace icp::config::file {
 // Function to check if a given input string is
 // either a decimal or integer number.
 // Not an exhaustive checker (only supports base-10).
-bool icp_config_is_number(const std::string &value);
+bool icp_config_is_number(const std::string_view value);
 
 // Function to split a resource path from an identifier.
 // In Inception it is assumed that /<path>/<id> in a configuration file maps to
 // a REST endpoint at /<path> and a user-defined identifier, <id>.
 // <path> may also include multiple forward slashes.
-std::tuple<std::string, std::string> icp_config_split_path_id(const std::string &path_id);
+// Function requires a null-terminated string. Otherwise behavior is undefined.
+std::tuple<std::string_view, std::string_view> icp_config_split_path_id(std::string_view path_id);
 
 }  // namespace icp::config::file
 
