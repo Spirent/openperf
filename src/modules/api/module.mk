fafac0c2707ac52ebec2e3765affed650c233e6b
Timmons C. Player <timmons.player@spirent.com>
2019-05-07 20:29:12 +0000

    Clean up build variables
    * Use the CircleCI build number if present as the build number.
    * Add an explicit version based on the latest git tag.
    * Clean up and simplify build variable usage

--- src/modules/api/module.mk
+++ src/modules/api/module.mk
@@ -11,18 +11,6 @@ API_SOURCES += \
 	api_register.c \
 	api_version.cpp
 
-API_TEST_DEPENDS += pistache
-
-API_TEST_SOURCES += \
-	api_init.cpp \
-	api_internal_client.cpp
-
-.PHONY: $(API_SRC_DIR)/api_version.cpp
-$(API_OBJ_DIR)/api_version.o: ICP_CPPFLAGS += \
-	-DVERSION_COMMIT="\"$(GIT_COMMIT)\"" \
-	-DVERSION_NUMBER="\"$(BUILD_NUMBER)\"" \
-	-DVERSION_TIMESTAMP="\"$(TIMESTAMP)\""
-
 .PHONY: $(API_SRC_DIR)/api_init.cpp
 $(API_OBJ_DIR)/api_init.o: ICP_CPPFLAGS += \
 	-DBUILD_COMMIT="\"$(GIT_COMMIT)\"" \
@@ -30,12 +18,19 @@ $(API_OBJ_DIR)/api_init.o: ICP_CPPFLAGS += \
 	-DBUILD_TIMESTAMP="\"$(TIMESTAMP)\""
 
 .PHONY: $(API_SRC_DIR)/api_version.cpp
-$(API_TEST_OBJ_DIR)/api_version.o: ICP_CPPFLAGS += \
-	-DVERSION_COMMIT="\"$(GIT_COMMIT)\"" \
-	-DVERSION_NUMBER="\"$(BUILD_NUMBER)\"" \
-	-DVERSION_TIMESTAMP="\"$(TIMESTAMP)\""
+$(API_OBJ_DIR)/api_version.o: ICP_CPPFLAGS += \
+	-DBUILD_COMMIT="\"$(GIT_COMMIT)\"" \
+	-DBUILD_NUMBER="\"$(BUILD_NUMBER)\"" \
+	-DBUILD_TIMESTAMP="\"$(TIMESTAMP)\"" \
+	-DBUILD_VERSION="\"$(GIT_VERSION)\""
 
-.PHONY: $(API_SRC_DIR)/api_init.cpp
+API_TEST_DEPENDS += pistache
+
+API_TEST_SOURCES += \
+	api_init.cpp \
+	api_internal_client.cpp
+
+.PHONY: $(API_TEST_SRC_DIR)/api_init.cpp
 $(API_TEST_OBJ_DIR)/api_init.o: ICP_CPPFLAGS += \
 	-DBUILD_COMMIT="\"$(GIT_COMMIT)\"" \
 	-DBUILD_NUMBER="\"$(BUILD_NUMBER)\"" \
