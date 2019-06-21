20a376287e6156e2d748696731a8aaaad3f62f1b
Dan Morton <daniel.morton@spirent.com>
2019-06-21 10:39:31 -1000

    Add additional AATs (#112)
    * Additional port and interface AAT cases, including loading from a 
    configuration file.
    
    * Fix a pair of minor issues with user-defined ID support.
    
    * AATs now create a single log file per run and each launch of the service appends to it.
    Previous log file, if any, is removed by makefile prior to starting tests.

--- tests/aat/common/launcher.sh
+++ tests/aat/common/launcher.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+printf "\n***Starting Service***\n"
+
 check_command="/sbin/setcap -q -v CAP_IPC_LOCK,CAP_NET_RAW,CAP_SYS_ADMIN,CAP_DAC_OVERRIDE,CAP_SYS_PTRACE=epi $1"
 
 if $check_command; then
