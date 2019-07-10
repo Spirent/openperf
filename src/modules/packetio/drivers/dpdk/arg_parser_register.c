8e7420cacae114c23b1615cf27e8c66d7a441771
Daniel Morton <daniel.morton@spirent.com>
2019-07-09 14:21:22 -1000

    Update to use the new std::optional return type from the unified configuration framework
    Also code cleanup.

--- src/modules/packetio/drivers/dpdk/arg_parser_register.c
+++ src/modules/packetio/drivers/dpdk/arg_parser_register.c
@@ -2,34 +2,18 @@
 #include "config/icp_config_file.h"
 
 extern int dpdk_arg_parse_init();
-/* extern int dpdk_arg_parse_handler(int opt, const char *opt_arg); */
 
 MAKE_OPTION_DATA(
   dpdk, dpdk_arg_parse_init,
-  MAKE_OPT("quoted, comma separated options for DPDK", "modules.packetio.dpdk.options", 'd',
+  MAKE_OPT("quoted, comma separated options for DPDK",
+           "modules.packetio.dpdk.options", 'd',
            ICP_OPTION_TYPE_LIST),
-  MAKE_OPT("enable test mode by creating loopback port pairs", "modules.packetio.dpdk.test-mode", 0,
-           ICP_OPTION_TYPE_BOOL),
+  MAKE_OPT("enable test mode by creating loopback port pairs",
+           "modules.packetio.dpdk.test-mode", 0,
+           ICP_OPTION_TYPE_NONE),
   MAKE_OPT("number of loopback port pairs for testing, defaults to 1",
            "modules.packetio.dpdk.test-portpairs", 0, ICP_OPTION_TYPE_LONG),
   MAKE_OPT("quoted, comma separated list of port index-id mappings in the form portX=id",
            "modules.packetio.dpdk.port-ids", 0, ICP_OPTION_TYPE_MAP), );
 
 REGISTER_CLI_OPTIONS(dpdk)
-
-/*
-struct icp_options_data dpdk_options = {
-    .name = "DPDK",
-    .init = dpdk_arg_parse_init,
-    .callback = dpdk_arg_parse_handler,
-    .options = {
-        {"quoted, comma separated options for DPDK", "dpdk", 'd', ICP_OPTION_TYPE_STRING},
-        {"enable test mode by creating loopback port pairs", "dpdk-test-mode", 0, ICP_OPTION_TYPE_NONE},
-        {"number of loopback port pairs for testing, defaults to 1", "dpdk-test-portpairs", 0, ICP_OPTION_TYPE_LONG},
-        {"quoted, comma separated list of port index-id mappings in the form portX=id", "dpdk-port-ids", 0, ICP_OPTION_TYPE_MAP},
-        { 0, 0, 0, 0 },
-    },
-};
-
-REGISTER_OPTIONS(dpdk_options)
-*/
