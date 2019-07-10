8ba3b43fd155dbb0895b02383fcef32a303046ea
Daniel Morton <daniel.morton@spirent.com>
2019-07-09 14:21:22 -1000

    Update to use the new std::optional return type from the unified configuration framework
    Also code cleanup.

--- src/modules/packetio/drivers/dpdk/arg_parser.h
+++ src/modules/packetio/drivers/dpdk/arg_parser.h
@@ -5,9 +5,7 @@
 #include <vector>
 #include <unordered_map>
 
-namespace icp {
-namespace packetio {
-namespace dpdk {
+namespace icp::packetio::dpdk {
 
 template <typename T>
 class singleton {
@@ -29,23 +27,17 @@ class arg_parser : public singleton<arg_parser>
 {
 public:
     int init(const char *name);                    /**< Initialize args vector */
-    //int parse(int opt, const char *opt_arg);       /**< Parse arguments */
     int test_portpairs();                          /**< Number of eth ring devs */
     bool test_mode();                              /**< test mode enable/disable */
     std::vector<std::string> args();               /**< Retrieve a copy of args for use */
     std::unordered_map<int, std::string> id_map(); /**< Retrieve a copy of port idx->id map */
 
 private:
-    std::vector<std::string> _args;
     std::string m_name;
-    //int m_test_portpairs;
-    //bool m_test_mode;
-    //std::unordered_map<int, std::string> m_port_index_id;
 };
 
 
-}
-}
-}
+} /* namespace icp::packetio::dpdk */
+
 
 #endif /* _ICP_PACKETIO_DPDK_ARG_PARSER_H_ */
