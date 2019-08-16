8e59b9ac0b3c1290106966e3113d59cfc38b5c50
Timmons C. Player <timmons.player@spirent.com>
2019-08-16 21:53:46 +0000

    build: add proper dependencies for versioned files
    Multiple files within this project use build variables as dependencies
    to hard code version information into the objects at compile time.
    Previously, those files were compiled at ever invocation of `make`.
    This change updates the build infrastructure so that the files that
    contain the version information will only be build if there are actual
    changes to the files they seek to version.

--- src/framework/config/directory.mk
+++ src/framework/config/directory.mk
@@ -1,13 +1,12 @@
+#
+# Makefile component for YAML based configuration files
+#
+
+CFGFILE_DEPENDS += yaml_cpp
+
 CFGFILE_SOURCES += \
 	config/icp_config_file.cpp \
 	config/icp_config_file_options.c \
 	config/icp_config_file_utils.cpp \
 	config/icp_config_utils.cpp \
 	config/yaml_json_emitter.cpp
-
-CFGFILE_DEPENDS += yaml_cpp
-
-CFGFILETEST_SOURCES += \
-	$(CFGFILE_SOURCES)
-
-CFGFILETEST_DEPENDS += yaml_cpp
