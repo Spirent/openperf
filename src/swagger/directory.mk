6846479c639f639a07634a5791a26dc1901f3706
Timmons C. Player <timmons.player@spirent.com>
2019-12-05 15:32:58 +0000

    swagger: simplify directory makefile

--- src/swagger/directory.mk
+++ src/swagger/directory.mk
@@ -1,31 +1,6 @@
 SWAGGER_DEPENDS += json
 SWAGGER_INCLUDES += v1/model
 
-MODEL_SOURCES := \
-	BulkCreateInterfacesRequest.cpp \
-	BulkCreateInterfacesResponse.cpp \
-	BulkDeleteInterfacesRequest.cpp \
-	Interface_config.cpp \
-	Interface.cpp \
-	InterfaceProtocolConfig.cpp \
-	InterfaceProtocolConfig_eth.cpp \
-	InterfaceProtocolConfig_ipv4.cpp \
-	InterfaceProtocolConfig_ipv4_dhcp.cpp \
-	InterfaceProtocolConfig_ipv4_static.cpp \
-	InterfaceStats.cpp \
-	ModelBase.cpp \
-	Module.cpp \
-	ModuleVersion.cpp \
-	PortConfig_bond.cpp \
-	PortConfig.cpp \
-	PortConfig_dpdk.cpp \
-	Port.cpp \
-	PortStats.cpp \
-	PortStatus.cpp \
-	Stack.cpp \
-	StackElementStats.cpp \
-	StackMemoryStats.cpp \
-	StackProtocolStats.cpp \
-	StackStats.cpp
-
-SWAGGER_SOURCES += $(addprefix v1/model/,$(MODEL_SOURCES))
+# Just suck up every cpp file in the model directory properly formatted
+# for our generic build rules
+SWAGGER_SOURCES += $(patsubst $(SWAGGER_SRC_DIR)/%,%,$(wildcard $(SWAGGER_SRC_DIR)/v1/model/*.cpp))
