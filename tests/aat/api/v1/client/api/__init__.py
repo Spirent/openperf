781e8dbab2b0566ca092088a782ac5859c9ef4e4
Timmons C. Player <timmons.player@spirent.com>
2020-02-12 04:12:54 +0000

    api: unify the timesync API
    Replace the three tags used to identify timesync REST paths with a
    single tag.  Update unit test client code.

--- tests/aat/api/v1/client/api/__init__.py
+++ tests/aat/api/v1/client/api/__init__.py
@@ -7,6 +7,4 @@ from client.api.interfaces_api import InterfacesApi
 from client.api.modules_api import ModulesApi
 from client.api.ports_api import PortsApi
 from client.api.stacks_api import StacksApi
-from client.api.time_counters_api import TimeCountersApi
-from client.api.time_keeper_api import TimeKeeperApi
-from client.api.time_sources_api import TimeSourcesApi
+from client.api.time_sync_api import TimeSyncApi
