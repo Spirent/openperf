0e3f0820bb67758abbbae4fa3fc56670c42dad1d
Charles Myers <charles.myers@spirent.com>
2020-02-21 13:02:20 -1000

    tests/aat: Support for swagger 2.4.12 and python3
    Swagger template files were extracted from the 2.4.12 jar file
    and model.mustache changes made to 2.3.1 were added back for 2.4.12
    The rest of the template files are unchanged from 2.4.12.

--- tests/aat/common/helper/__init__.py
+++ tests/aat/common/helper/__init__.py
@@ -4,4 +4,5 @@ from __future__ import absolute_import
 from common.helper.interface import (empty_interface,
                                      example_interface,
                                      ipv4_interface,
-                                     as_interface_protocol)
+                                     as_interface_protocol,
+                                     make_interface_protocols)
