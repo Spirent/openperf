36bd3fb2b6077df82fc2e0c9b99e9c6a60e6af0f
Artem Belov <artem.belov@xored.com>
2020-03-04 21:47:44 +0700

    Kepp block files in map
    Signed-off-by: Artem Belov <artem.belov@xored.com>

--- src/modules/block/block_file.hpp
+++ src/modules/block/block_file.hpp
@@ -2,20 +2,23 @@
 #define _OP_BLOCK_FILE_HPP_
 
 #include "swagger/v1/model/BlockFile.h"
+#include "tl/expected.hpp"
 
 using namespace swagger::v1::model;
 
 namespace openperf::block::file {
-    
+
+typedef std::map<std::string, std::shared_ptr<BlockFile>> BlockFileMap;
+
 class block_file_stack {
 private: 
-    std::vector<std::shared_ptr<BlockFile>>  block_files;
+    BlockFileMap block_files;
 
 public:
     block_file_stack(){};
 
     std::vector<std::shared_ptr<BlockFile>> block_files_list();
-    std::shared_ptr<BlockFile> create_block_file(BlockFile& block_file);
+    tl::expected<std::shared_ptr<BlockFile>, std::string> create_block_file(BlockFile& block_file);
     std::shared_ptr<BlockFile> get_block_file(std::string id);
     void delete_block_file(std::string id);
 };
