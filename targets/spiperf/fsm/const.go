52ad0475db045d4f6ca189ec4a67f32f443fb1c0
Daniel Morton <Daniel.Morton@spirent.com>
2020-03-10 14:29:48 -1000

    targets/spiperf: add Server FSM
    Add Server FSM to complement the Client one.
    Also add corresponding unit tests.

--- targets/spiperf/fsm/const.go
+++ targets/spiperf/fsm/const.go
@@ -9,4 +9,8 @@ const (
 	DefaultStatsPollInterval     = 1 * time.Second
 	DefaultGeneratorPollInterval = 1 * time.Second
 	TimeFormatString             = time.RFC3339Nano
+
+	// MaximumStartTimeDelta maximum time in the future to start traffic.
+	// This value is somewhat arbitrary.
+	MaximumStartTimeDelta = 3 * time.Minute
 )
