5f8ea9e4a52d56b027910242285b23e98503b952
Artem Belov <artem.belov@spirent.com>
2020-03-24 23:09:52 +0700

    Refactor worker to use custom component
    Signed-off-by: Artem Belov <artem.belov@spirent.com>

--- src/modules/block/block_generator.hpp
+++ src/modules/block/block_generator.hpp
@@ -1,19 +1,22 @@
 #ifndef _OP_BLOCK_GENERATOR_HPP_
 #define _OP_BLOCK_GENERATOR_HPP_
 
+#include "task.hpp"
 #include "models/generator.hpp"
-#include "block/worker.hpp"
+#include "utils/worker/worker.hpp"
 
 namespace openperf::block::generator {
 
-using namespace openperf::block::worker; 
+using namespace openperf::block::worker;
+
+typedef utils::worker::worker<block_task> block_worker;
 typedef std::unique_ptr<block_worker> block_worker_ptr;
 
 class block_generator : public model::block_generator
 {
 private:
     block_worker_ptr blkworker;
-    worker_config generate_worker_config(const model::block_generator_config& generator_config, const size_t& resource_size);
+    task_config_t generate_worker_config();
     int open_resource(const std::string& resource_id);
     size_t get_resource_size(const std::string& resource_id);
 public:
@@ -21,7 +24,7 @@ public:
     block_generator(const model::block_generator& generator_model);
     void start();
     void stop();
-    
+
     void set_config(const model::block_generator_config& value);
     void set_resource_id(const std::string& value);
     void set_running(bool value);
