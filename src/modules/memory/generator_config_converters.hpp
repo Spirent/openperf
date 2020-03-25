639b10600899bdad90041a596d0e285c8c845987
Anatoly Loskutnikov <anatoly.loskutnikov@xored.com>
2020-03-25 19:07:18 +0700

    Remove excess code

--- src/modules/memory/generator_config_converters.hpp
+++ src/modules/memory/generator_config_converters.hpp
@@ -9,6 +9,30 @@ namespace openperf::memory {
 
 namespace swagger = ::swagger::v1::model;
 
+namespace {
+io_pattern pattern_from_string(const std::string& str)
+{
+    if (str == "random") return io_pattern::RANDOM;
+    if (str == "sequential") return io_pattern::SEQUENTIAL;
+    if (str == "reverse") return io_pattern::REVERSE;
+    return io_pattern::NONE;
+}
+
+std::string pattern_to_string(io_pattern pattern)
+{
+    switch (pattern) {
+    case io_pattern::RANDOM:
+        return "random";
+    case io_pattern::REVERSE:
+        return "reverse";
+    case io_pattern::SEQUENTIAL:
+        return "sequential";
+    default:
+        return "";
+    }
+}
+} // namespace
+
 generator_config swagger_model_to_config(const model::MemoryGeneratorConfig& m)
 {
     return generator_config::create()
@@ -18,7 +42,8 @@ generator_config swagger_model_to_config(const model::MemoryGeneratorConfig& m)
         .read_threads(m.getReadThreads())
         .write_size(m.getWriteSize())
         .writes_per_sec(m.getWritesPerSec())
-        .write_threads(m.getWriteThreads());
+        .write_threads(m.getWriteThreads())
+        .pattern(pattern_from_string(m.getPattern()));
 }
 
 swagger::MemoryGeneratorConfig
@@ -32,6 +57,7 @@ config_to_swagger_model(const generator_config& config)
     config_model.setWriteSize(config.write_size());
     config_model.setWritesPerSec(config.writes_per_sec());
     config_model.setWriteThreads(config.write_threads());
+    config_model.setPattern(pattern_to_string(config.pattern()));
 
     return config_model;
 }
