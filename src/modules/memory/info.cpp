6d583537ed06d1606534262e091add0ee27bf57a
Anatoly Loskutnikov <anatoly.loskutnikov@xored.com>
2020-04-08 16:20:34 +0700

    Migrate to binary-style API

--- src/modules/memory/info.cpp
+++ src/modules/memory/info.cpp
@@ -1,22 +1,20 @@
-#include "memory/info.hpp"
-
 #include <sys/sysinfo.h>
 
+#include "memory/info.hpp"
 #include "core/op_core.h"
 
 namespace openperf::memory {
 
-model::MemoryInfoResult memory_info::get()
+memory_info::info_t memory_info::get()
 {
     struct sysinfo info;
     memset(&info, 0, sizeof(info));
 
-    auto error = sysinfo(&info);
-    if (error == 0) {
-        model::MemoryInfoResult result;
-        result.setFreeMemory(info.freeram * info.mem_unit);
-        result.setTotalMemory(info.totalram * info.mem_unit);
-        return result;
+    if (sysinfo(&info) == 0) {
+        return info_t{
+            .total = info.totalram * info.mem_unit,
+            .free = info.freeram * info.mem_unit,
+        };
     }
 
     OP_LOG(OP_LOG_ERROR,
