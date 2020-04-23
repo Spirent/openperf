81d7e8b638580d722755587e214f1f0eec1fd205
Artem Belov <artem.belov@spirent.com>
2020-04-23 13:14:14 +0700

    Cpu info
    Signed-off-by: Artem Belov <artem.belov@spirent.com>

---
+++ src/modules/cpu/cpu_info.hpp
@@ -0,0 +1,19 @@
+#ifndef _OP_CPU_INFO_HPP_
+#define _OP_CPU_INFO_HPP_
+
+#include <unistd.h>
+#include <thread>
+
+#ifndef ARCH
+#define ARCH "unknown"
+#endif
+
+namespace openperf::cpu::info {
+
+int32_t cores_count();
+int64_t cache_line_size();
+std::string architecture();
+
+}
+
+#endif
