f2ff0532b469a41b0e6f42059f21b9f4759db12f
Timmons C. Player <timmons.player@spirent.com>
2020-06-04 18:45:23 +0000

    modules/packetio: simplify feature callbacks
    Replace feature callback boilerplate with a common template.
    
    No functional change.

--- src/modules/packetio/drivers/dpdk/port/signature_encoder.hpp
+++ src/modules/packetio/drivers/dpdk/port/signature_encoder.hpp
@@ -1,34 +1,20 @@
 #ifndef _OP_PACKETIO_DPDK_PORT_SIGNATURE_ENCODER_HPP_
 #define _OP_PACKETIO_DPDK_PORT_SIGNATURE_ENCODER_HPP_
 
-#include <map>
-
+#include "packetio/drivers/dpdk/port/callback.hpp"
 #include "packetio/drivers/dpdk/port/feature_toggle.hpp"
-
-struct rte_eth_rxtx_callback;
 
 namespace openperf::packetio::dpdk::port {
 
-class callback_signature_encoder
+struct callback_signature_encoder
+    : public tx_callback<callback_signature_encoder>
 {
-public:
-    callback_signature_encoder(uint16_t port_id);
-    ~callback_signature_encoder();
+    using parent_type = tx_callback<callback_signature_encoder>;
 
-    callback_signature_encoder(callback_signature_encoder&&) noexcept;
-    callback_signature_encoder&
-    operator=(callback_signature_encoder&&) noexcept;
-
-    uint16_t port_id() const;
-
-    void enable();
-    void disable();
-
-    using callback_map = std::map<uint16_t, const rte_eth_rxtx_callback*>;
-
-private:
-    callback_map m_callbacks;
-    uint16_t m_port;
+    using parent_type::tx_callback;
+    static std::string description();
+    static parent_type::tx_callback_fn callback();
+    static void* callback_arg();
 };
 
 struct signature_encoder
