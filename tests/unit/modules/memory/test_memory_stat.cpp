77c7c4d175ab518de859402ee1f12abb8b39ebf1
Anatoly Loskutnikov <anatoly.loskutnikov@xored.com>
2020-06-05 06:25:00 +0000

    Update memory generator unit test

--- tests/unit/modules/memory/test_memory_stat.cpp
+++ tests/unit/modules/memory/test_memory_stat.cpp
@@ -13,7 +13,9 @@ memory_stat random_memory_stat()
     stat.operations_target = random();
     stat.latency_max = std::chrono::nanoseconds(random());
     stat.latency_min = std::chrono::nanoseconds(random());
-    stat.time = std::chrono::nanoseconds(random());
+    stat.run_time = std::chrono::nanoseconds(random());
+    stat.sleep_time = std::chrono::nanoseconds(random());
+    stat.errors = random();
 
     return stat;
 }
@@ -32,7 +34,10 @@ TEST_CASE("memory stat addition", "[memory]")
         REQUIRE(st.operations == st1.operations + st2.operations);
         REQUIRE(st.operations_target
                 == st1.operations_target + st2.operations_target);
-        REQUIRE(st.time == st1.time + st2.time);
+        REQUIRE(st.run_time == st1.run_time + st2.run_time);
+        REQUIRE(st.sleep_time == st1.sleep_time + st2.sleep_time);
+        REQUIRE(st.errors == st1.errors + st2.errors);
+        REQUIRE(st.timestamp == st2.timestamp);
 
         REQUIRE(st.latency_min.has_value());
         REQUIRE(st.latency_min.value()
@@ -48,12 +53,14 @@ TEST_CASE("memory stat addition", "[memory]")
         auto st2 = random_memory_stat();
         auto st = st1 + st2;
 
-        REQUIRE(st.bytes == st1.bytes + st2.bytes);
-        REQUIRE(st.bytes_target == st1.bytes_target + st2.bytes_target);
-        REQUIRE(st.operations == st1.operations + st2.operations);
-        REQUIRE(st.operations_target
-                == st1.operations_target + st2.operations_target);
-        REQUIRE(st.time == st1.time + st2.time);
+        REQUIRE(st.bytes == st2.bytes);
+        REQUIRE(st.bytes_target == st2.bytes_target);
+        REQUIRE(st.operations == st2.operations);
+        REQUIRE(st.operations_target == st2.operations_target);
+        REQUIRE(st.run_time == st2.run_time);
+        REQUIRE(st.sleep_time == st2.sleep_time);
+        REQUIRE(st.errors == st2.errors);
+        REQUIRE(st.timestamp == st2.timestamp);
 
         REQUIRE(st.latency_min.has_value());
         REQUIRE(st.latency_min.value() == st2.latency_min.value());
