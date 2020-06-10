acfef73cd9d0ed5315ca923e832f13a16f84ed58
Timmons C. Player <timmons.player@spirent.com>
2020-06-10 18:43:25 +0000

    circleci: moar fixes
    * Uniformly limit memory bound jobs to 6 instances, max.
    * Fix a typo.

--- .circleci/run_build.sh
+++ .circleci/run_build.sh
@@ -3,7 +3,7 @@ set -xe
 # The container apparently sees the correct amount of memory, but not
 # not the correct number of cores.  If we use more than ~6 cores, we
 # seem to hit the memory limit and our build commands can get killed.
-CI_CORES=$(if [ $(nproc) -gt 6 ]; then echo 6; else echo $(nrpco); fi)
+CI_CORES=$(if [ $(nproc) -gt 6 ]; then echo 6; else echo $(nproc); fi)
 
 # Force optimizations to target avx2; all of the CircleCI machines seem
 # to support it and we don't want to get illegal instruction errors when
