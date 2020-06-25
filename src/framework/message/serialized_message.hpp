4c5c4f0189838aab3339e4b697cb0664e8a37654
Timmons C. Player <timmons.player@spirent.com>
2020-06-25 15:40:35 +0000

    framework/message: add 0MQ message framework
    Add a common 0MQ message framework for module message passing. The
    implementation is similar to the current code used by most modules, but
    incorporates proper RAII to clean up messages and supports arbitrary
    length multi-part messages.
    
    New modules should prefer this framework. Older modules should
    eventually update to it.

---
+++ src/framework/message/serialized_message.hpp
@@ -0,0 +1,197 @@
+#ifndef _OP_MESSAGE_SERIALIZED_MESSAGE_HPP_
+#define _OP_MESSAGE_SERIALIZED_MESSAGE_HPP_
+
+#include <memory>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include "zmq.h"
+#include "tl/expected.hpp"
+
+namespace openperf::message {
+
+struct serialized_message
+{
+    std::vector<zmq_msg_t> parts;
+
+    serialized_message() = default;
+
+    serialized_message(serialized_message&& other)
+        : parts(std::move(other.parts))
+    {}
+
+    serialized_message& operator=(serialized_message&& other)
+    {
+        if (this != &other) { parts = std::move(other.parts); }
+        return (*this);
+    }
+
+    ~serialized_message()
+    {
+        std::for_each(std::begin(parts), std::end(parts), [](auto& part) {
+            zmq_msg_close(&part);
+        });
+    }
+};
+
+inline void pop_front(serialized_message& msg)
+{
+    auto& front = msg.parts.front();
+    zmq_msg_close(&front);
+    msg.parts.erase(std::begin(msg.parts));
+}
+
+template <typename T> auto push(serialized_message& msg, const T& value)
+{
+    auto& part = msg.parts.emplace_back();
+
+    if (auto error = zmq_msg_init_size(&part, sizeof(T)); error != 0) {
+        msg.parts.pop_back();
+        return (error);
+    }
+
+    auto ptr = reinterpret_cast<T*>(zmq_msg_data(&part));
+    *ptr = value;
+    return (0);
+}
+
+template <typename T>
+auto push(serialized_message& msg, std::unique_ptr<T> value)
+{
+    auto& part = msg.parts.emplace_back();
+
+    if (auto error = zmq_msg_init_size(&part, sizeof(T)); error != 0) {
+        msg.parts.pop_back();
+        return (error);
+    }
+
+    auto ptr = reinterpret_cast<T**>(zmq_msg_data(&part));
+    *ptr = value.release();
+    return (0);
+}
+
+template <typename T>
+auto push(serialized_message& msg, const T* buffer, size_t length)
+{
+    auto& part = msg.parts.emplace_back();
+
+    if (auto error = zmq_msg_init_size(&part, length); error != 0) {
+        msg.parts.pop_back();
+        return (error);
+    }
+
+    auto ptr = reinterpret_cast<char*>(zmq_msg_data(&part));
+    auto src = reinterpret_cast<const char*>(buffer);
+    std::copy(src, src + length, ptr);
+    return (0);
+}
+
+template <typename T>
+auto push(serialized_message& msg, std::vector<std::unique_ptr<T>>& values)
+{
+    auto& part = msg.parts.emplace_back();
+
+    if (auto error = zmq_msg_init_size(&part, sizeof(T*) * values.size());
+        error != 0) {
+        msg.parts.pop_back();
+        return (error);
+    }
+
+    auto cursor = reinterpret_cast<T**>(zmq_msg_data(&part));
+    std::transform(std::begin(values), std::end(values), cursor, [](auto& ptr) {
+        return (ptr.release());
+    });
+    return (0);
+}
+
+template <typename T>
+std::enable_if_t<!std::is_pointer_v<T>, T> pop(serialized_message& msg)
+{
+    if (msg.parts.empty()) { return T{}; }
+
+    auto& front = msg.parts.front();
+    auto obj = *(reinterpret_cast<T*>(zmq_msg_data(&front)));
+    pop_front(msg);
+    return (obj);
+}
+
+template <typename T>
+std::enable_if_t<std::is_pointer_v<T>, T> pop(serialized_message& msg)
+{
+    if (msg.parts.empty()) { return (nullptr); }
+
+    auto& front = msg.parts.front();
+    auto* ptr = *(reinterpret_cast<T*>(zmq_msg_data(&front)));
+    pop_front(msg);
+    return (ptr);
+}
+
+inline std::string pop_string(serialized_message& msg)
+{
+    if (msg.parts.empty()) { return {}; };
+
+    auto& front = msg.parts.front();
+    auto str = std::string(reinterpret_cast<char*>(zmq_msg_data(&front)),
+                           zmq_msg_size(&front));
+    pop_front(msg);
+    return (str);
+}
+
+template <typename T>
+std::vector<std::unique_ptr<T>> pop_unique_vector(serialized_message& msg)
+{
+    if (msg.parts.empty()) { return {}; }
+
+    auto& front = msg.parts.front();
+    auto size = zmq_msg_size(&front) / sizeof(T*);
+    auto data = reinterpret_cast<T**>(zmq_msg_data(&front));
+
+    auto vec = std::vector<std::unique_ptr<T>>{};
+    std::for_each(
+        data, data + size, [&](const auto& ptr) { vec.emplace_back(ptr); });
+    pop_front(msg);
+    return (vec);
+}
+
+inline int send(void* socket, serialized_message&& msg)
+{
+    if (any_of(std::begin(msg.parts),
+               std::prev(std::end(msg.parts)),
+               [&](auto&& part) {
+                   return (zmq_msg_send(&part, socket, ZMQ_SNDMORE) == -1);
+               })) {
+        return (errno);
+    }
+
+    return (zmq_msg_send(&msg.parts.back(), socket, 0) == -1 ? errno : 0);
+}
+
+inline tl::expected<serialized_message, int> recv(void* socket, int flags)
+{
+    auto msg = serialized_message{};
+    auto more = int{0};
+
+    do {
+        auto& part = msg.parts.emplace_back();
+        if (zmq_msg_init(&part) == -1) { return (tl::make_unexpected(ENOMEM)); }
+        if (zmq_msg_recv(&part, socket, flags) == -1) {
+            return (tl::make_unexpected(errno));
+        }
+        auto more_size = sizeof(more);
+        if (zmq_getsockopt(socket, ZMQ_RCVMORE, &more, &more_size) == -1) {
+            return (tl::make_unexpected(errno));
+        };
+    } while (more);
+
+    return (msg);
+}
+
+inline tl::expected<serialized_message, int> recv(void* socket)
+{
+    return (recv(socket, 0));
+}
+
+} // namespace openperf::message
+
+#endif /* _OP_MESSAGE_SERIALIZED_MESSAGE_HPP_ */
