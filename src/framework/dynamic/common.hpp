bd589e5f79ddf3e9116106837de300247eadbd47
Anatoly Loskutnikov <anatoly.loskutnikov@xored.com>
2020-06-30 01:31:56 +0900

    Rename inspector to poller

--- src/framework/dynamic/common.hpp
+++ src/framework/dynamic/common.hpp
@@ -5,23 +5,12 @@
 
 namespace openperf::dynamic {
 
-struct dynamic_argument
+struct argument
 {
     enum function_t { DX = 1, DXDY, DXDT };
     std::string x;
     std::string y;
     function_t function;
-
-    // dynamic_argument(std::string_view x_name, function_t f = DX)
-    //    : x(x_name)
-    //    , function(f)
-    //{}
-
-    // dynamic_argument(std::string_view x_name, std::string_view y_name)
-    //    : x(x_name)
-    //    , y(y_name)
-    //    , function(DXDY)
-    //{}
 };
 
 } // namespace openperf::dynamic
