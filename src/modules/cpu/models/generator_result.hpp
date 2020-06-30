e8beff198fe9fa67fed0df8fe94e23245668ec84
Anatoly Loskutnikov <anatoly.loskutnikov@xored.com>
2020-06-30 11:26:50 +0000

    Integrate Dynamic to CPU

--- src/modules/cpu/models/generator_result.hpp
+++ src/modules/cpu/models/generator_result.hpp
@@ -34,12 +34,12 @@ public:
 
     double field(std::string_view name) const
     {
-        if (name == "available") return m_stats.available.count();
-        if (name == "utilization") return m_stats.utilization.count();
-        if (name == "system") return m_stats.system.count();
-        if (name == "user") return m_stats.user.count();
-        if (name == "steal") return m_stats.steal.count();
-        if (name == "error") return m_stats.error.count();
+        if (name == "available") return available.count();
+        if (name == "utilization") return utilization.count();
+        if (name == "system") return system.count();
+        if (name == "user") return user.count();
+        if (name == "steal") return steal.count();
+        if (name == "error") return error.count();
 
         constexpr auto prefix = "cores[";
         auto prefix_size = std::strlen(prefix);
@@ -48,22 +48,22 @@ public:
                 prefix_size,
                 name.find_first_of(']', prefix_size) - prefix_size)));
 
-            if (core_number < m_stats.cores.size()) {
+            if (core_number < cores.size()) {
                 auto field_name =
                     name.substr(name.find_first_of('.', prefix_size) + 1);
 
                 if (field_name == "available")
-                    return m_stats.cores[core_number].available.count();
+                    return cores[core_number].available.count();
                 if (field_name == "utilization")
-                    return m_stats.cores[core_number].utilization.count();
+                    return cores[core_number].utilization.count();
                 if (field_name == "system")
-                    return m_stats.cores[core_number].system.count();
+                    return cores[core_number].system.count();
                 if (field_name == "user")
-                    return m_stats.cores[core_number].user.count();
+                    return cores[core_number].user.count();
                 if (field_name == "steal")
-                    return m_stats.cores[core_number].steal.count();
+                    return cores[core_number].steal.count();
                 if (field_name == "error")
-                    return m_stats.cores[core_number].error.count();
+                    return cores[core_number].error.count();
             }
         }
 
@@ -84,7 +84,7 @@ public:
                 prefix_size,
                 name.find_first_of(']', prefix_size) - prefix_size)));
 
-            if (core_number >= m_stats.cores.size()) return false;
+            if (core_number >= cores.size()) return false;
 
             auto field_name = std::string(
                 name.substr(name.find_first_of('.', prefix_size) + 1));
@@ -94,6 +94,28 @@ public:
 
         return false;
     }
+};
+
+class generator_result
+{
+public:
+    generator_result() = default;
+    generator_result(const generator_result&) = default;
+
+    std::string id() const { return m_id; }
+    void id(std::string_view id) { m_id = id; }
+
+    std::string generator_id() const { return m_generator_id; }
+    void generator_id(std::string_view id) { m_generator_id = id; }
+
+    bool active() const { return m_active; }
+    void active(bool active) { m_active = active; }
+
+    generator_stats stats() const { return m_stats; }
+    void stats(const generator_stats& stats) { m_stats = stats; }
+
+    dynamic::results dynamic_results() const { return m_dynamic_results; }
+    void dynamic_results(const dynamic::results& r) { m_dynamic_results = r; }
 
 protected:
     std::string m_id;
@@ -101,6 +123,7 @@ protected:
     bool m_active;
     time_point m_timestamp;
     cpu_stat m_stats;
+    dynamic::results m_dynamic_results;
 };
 
 } // namespace openperf::cpu::model
