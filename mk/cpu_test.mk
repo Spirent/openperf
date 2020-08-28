cb405e00f3703ec974ca8e12506050c6ffd5e146
Anatoly Loskutnikov <anatoly.loskutnikov@xored.com>
2020-08-28 19:23:40 +0900

    Add unit tests
    Signed-off-by: Anatoly Loskutnikov <anatoly.loskutnikov@xored.com>

--- mk/cpu_test.mk
+++ mk/cpu_test.mk
@@ -26,9 +26,9 @@ CPU_TEST_OBJECTS := $(call op_generate_objects,$(CPU_TEST_SOURCES),$(CPU_TEST_OB
 CPU_TEST_ISPC_TARGETS := \
 		avx1-i32x8 \
 		avx2-i32x8 \
-		avx512skx-i32x8 \
-		sse2-i32x8 \
-		sse4-i32x8
+		avx512skx-i32x16 \
+		sse2-i32x4 \
+		sse4-i32x4
 
 ###
 # ISPC generates multiple object files when using multiple targets.
@@ -42,7 +42,6 @@ CPU_TEST_ISPC_OBJECTS := $(patsubst %, $(CPU_TEST_OBJ_DIR)/%, \
 
 # Adjust objects based on the number of ISPC target architectures
 CPU_TEST_DEFINES :=
-CPU_TEST_ISPC_DEFINES := CPU_PARALLELIZE_ALGORITHM
 CPU_TEST_ISPC_TARGET_OBJECTS :=
 
 ifeq (,$(word 2,$(CPU_TEST_ISPC_TARGETS)))  # i.e. there is no 2nd target
@@ -71,27 +70,32 @@ else
 	endif
 endif
 
-CPU_TEST_FLAGS := -DARCH="\"$(ARCH)\"" -mpclmul $(addprefix -D,$(CPU_TEST_DEFINES))
+CPU_TEST_FLAGS := -mpclmul $(addprefix -D,$(CPU_TEST_DEFINES))
 CPU_TEST_LIBRARY := openperf_cpu_test
 CPU_TEST_TARGET := $(CPU_TEST_LIB_DIR)/lib$(CPU_TEST_LIBRARY).a
 
 OP_LDLIBS += -Wl,--whole-archive -l$(CPU_TEST_LIBRARY) -Wl,--no-whole-archive $(CPU_TEST_LDLIBS)
 
-# Load external dependencies
 -include $(CPU_TEST_OBJECTS:.o=.d)
+-include $(CPU_TEST_ISPC_OBJECTS:.o=.d)
+
+# Load external dependencies
 $(call op_include_dependencies,$(CPU_TEST_DEPENDS))
 
 ###
 # Build rules
 ###
 $(eval $(call op_generate_build_rules,$(CPU_TEST_SOURCES),CPU_TEST_SRC_DIR,CPU_TEST_OBJ_DIR,CPU_TEST_DEPENDS,CPU_TEST_FLAGS))
-$(eval $(call op_generate_clean_rules,cpu,CPU_TEST_TARGET,CPU_TEST_OBJECTS))
+$(eval $(call op_generate_clean_rules,cpu_test,CPU_TEST_TARGET,CPU_TEST_OBJECTS))
 
 # ISPC options
 CPU_TEST_ISPC_OPTS :=
 ifneq ($(MODE),release)
 	CPU_TEST_ISPC_OPTS += -g
 endif
+
+# The ISPC target objects depend on their source "objects"
+$(CPU_TEST_ISPC_TARGET_OBJECTS): $(CPU_TEST_ISPC_OBJECTS)
 
 # In addition to the standard rules above, we need a couple of ISPC rules
 # Note: that the sed command on the bottom is because ispc doesn't write
@@ -103,8 +107,7 @@ CPU_TEST_SPACE := $(CPU_TEST_EMPTY) $(CPU_TEST_EMPTY)
 
 $(CPU_TEST_OBJ_DIR)/%.o: $(CPU_TEST_SRC_DIR)/%.ispc
 	@mkdir -p $(dir $@)
-	$(strip $(OP_ISPC) $(addprefix -D,$(CPU_TEST_ISPC_DEFINES)) \
-		-o $@ $(CPU_TEST_ISPC_OPTS) \
+	$(strip $(OP_ISPC) -o $@ $(CPU_TEST_ISPC_OPTS) \
 		-M -MF $(@:.o=.d) \
 		--target=$(subst $(CPU_TEST_SPACE),$(CPU_TEST_COMMA),$(CPU_TEST_ISPC_TARGETS)) \
 		--header-outfile=$(patsubst %,$(CPU_TEST_OBJ_DIR)/%,$(notdir $<.h)) $<)
