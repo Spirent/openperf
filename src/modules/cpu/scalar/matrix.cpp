e5df5083105d2109262d628515fa627f1e14fc48
Anatoly Loskutnikov <anatoly.loskutnikov@xored.com>
2020-09-01 01:48:11 +0900

    Replace macros to template

--- src/modules/cpu/scalar/matrix.cpp
+++ src/modules/cpu/scalar/matrix.cpp
@@ -4,23 +4,49 @@
 
 namespace openperf::cpu::internal::scalar {
 
-#define DEFINE_MULTIPLICATE_MATRIX(suffix, type)                               \
-    void multiplicate_matrix_##suffix(                                         \
-        type* matrix_a, type* matrix_b, type* matrix_r, uint32_t size)         \
-    {                                                                          \
-        for (uint32_t i = 0; i < size; i++) {                                  \
-            for (uint32_t j = 0; j < size; j++) {                              \
-                type sum = 0;                                                  \
-                for (uint32_t k = 0; k < size; k++)                            \
-                    sum += matrix_a[i * size + k] * matrix_b[k * size + j];    \
-                matrix_r[i * size + j] = sum;                                  \
-            }                                                                  \
-        }                                                                      \
+template <typename T>
+void multiplicate_matrix(T* matrix_a, T* matrix_b, T* matrix_r, uint32_t size)
+{
+    for (uint32_t i = 0; i < size; i++) {
+        for (uint32_t j = 0; j < size; j++) {
+            T sum = 0;
+            for (uint32_t k = 0; k < size; k++)
+                sum += matrix_a[i * size + k] * matrix_b[k * size + j];
+            matrix_r[i * size + j] = sum;
+        }
     }
+}
 
-DEFINE_MULTIPLICATE_MATRIX(float, float)
-DEFINE_MULTIPLICATE_MATRIX(double, double)
-DEFINE_MULTIPLICATE_MATRIX(uint32, uint32_t)
-DEFINE_MULTIPLICATE_MATRIX(uint64, uint64_t)
+void multiplicate_matrix_float(float* matrix_a,
+                               float* matrix_b,
+                               float* matrix_r,
+                               uint32_t size)
+{
+    multiplicate_matrix<float>(matrix_a, matrix_b, matrix_r, size);
+}
+
+void multiplicate_matrix_double(double* matrix_a,
+                                double* matrix_b,
+                                double* matrix_r,
+                                uint32_t size)
+{
+    multiplicate_matrix<double>(matrix_a, matrix_b, matrix_r, size);
+}
+
+void multiplicate_matrix_uint32(uint32_t* matrix_a,
+                                uint32_t* matrix_b,
+                                uint32_t* matrix_r,
+                                uint32_t size)
+{
+    multiplicate_matrix<uint32_t>(matrix_a, matrix_b, matrix_r, size);
+}
+
+void multiplicate_matrix_uint64(uint64_t* matrix_a,
+                                uint64_t* matrix_b,
+                                uint64_t* matrix_r,
+                                uint32_t size)
+{
+    multiplicate_matrix<uint64_t>(matrix_a, matrix_b, matrix_r, size);
+}
 
 } // namespace openperf::cpu::internal::scalar
