6cf96bda896216554f13940f027b9193b1d426f8
Anatoly Loskutnikov <anatoly.loskutnikov@gmail.com>
2020-12-08 04:19:08 +0000

    tests/aat: add cpu stats values checks

--- tests/aat/common/matcher/cpu.py
+++ tests/aat/common/matcher/cpu.py
@@ -1,6 +1,7 @@
-from expects import expect, be_a, be_empty, be_none, be_below_or_equal
+from expects import expect, be_a, be_empty, be_none, be_below_or_equal, equal
 from expects.matchers import Matcher
 import client.models
+
 
 class _be_valid_cpu_info(Matcher):
     def _match(self, info):
@@ -10,6 +11,7 @@ class _be_valid_cpu_info(Matcher):
         expect(info.architecture).to(be_a(str))
         return True, ['is valid CPU Info']
 
+
 class _be_valid_cpu_generator(Matcher):
     def _match(self, g7r):
         expect(g7r).to(be_a(client.models.CpuGenerator))
@@ -17,6 +19,7 @@ class _be_valid_cpu_generator(Matcher):
         expect(g7r.running).to(be_a(bool))
         expect(g7r.config).to(be_valid_cpu_generator_config)
         return True, ['is valid CPU Generator']
+
 
 class _be_valid_cpu_generator_config(Matcher):
     def _match(self, conf):
@@ -31,11 +34,13 @@ class _be_valid_cpu_generator_config(Matcher):
             return False, ['unknown configuration method']
         return True, ['is valid CPU Generator Configuration']
 
+
 class _be_valid_cpu_generator_system_config(Matcher):
     def _match(self, conf):
         expect(conf).to(be_a(client.models.CpuGeneratorSystemConfig))
         expect(conf.utilization).to(be_a(float))
         return True, ['is valid CPU Generator System Configuration']
+
 
 class _be_valid_cpu_generator_core_config(Matcher):
     def _match(self, conf):
@@ -46,6 +51,7 @@ class _be_valid_cpu_generator_core_config(Matcher):
             expect(t).to(be_valid_cpu_generator_core_config_targets)
         return True, ['is valid CPU Generator Core Configuration']
 
+
 class _be_valid_cpu_generator_core_config_targets(Matcher):
     def _match(self, target):
         expect(target).to(be_a(client.models.CpuGeneratorCoreConfigTargets))
@@ -53,6 +59,7 @@ class _be_valid_cpu_generator_core_config_targets(Matcher):
         expect(target.data_type).to(be_a(str))
         expect(target.weight).to(be_a(int))
         return True, ['is valid CPU Generator Core Targets Configuration']
+
 
 class _be_valid_cpu_generator_result(Matcher):
     def _match(self, result):
@@ -63,8 +70,27 @@ class _be_valid_cpu_generator_result(Matcher):
         expect(result.timestamp_first).not_to(be_none)
         expect(result.timestamp_last).not_to(be_none)
         expect(result.timestamp_first).to(be_below_or_equal(result.timestamp_last))
-        expect(result.stats).to(be_a(client.models.CpuGeneratorStats))
+        expect(result.stats).to(be_valid_cpu_generator_stats)
         return True, ['is valid CPU Generator Result']
+
+
+class _be_valid_cpu_generator_core_stats(Matcher):
+    def _match(self, stats):
+        expect(stats).to(be_a(client.models.CpuGeneratorCoreStats))
+        expect(stats.utilization).to(equal(stats.system + stats.user - stats.steal))
+        expect(stats.error).to(equal(stats.target - stats.utilization))
+        return True, ['is valid CPU Generator Core Stats']
+
+
+class _be_valid_cpu_generator_stats(Matcher):
+    def _match(self, stats):
+        expect(stats).to(be_a(client.models.CpuGeneratorStats))
+        expect(stats.utilization).to(equal(stats.system + stats.user - stats.steal))
+        expect(stats.error).to(equal(stats.target - stats.utilization))
+        for core in stats.cores:
+            expect(core).to(be_valid_cpu_generator_core_stats)
+        return True, ['is valid CPU Generator Stats']
+
 
 be_valid_cpu_info = _be_valid_cpu_info()
 be_valid_cpu_generator = _be_valid_cpu_generator()
@@ -73,3 +99,5 @@ be_valid_cpu_generator_system_config = _be_valid_cpu_generator_system_config()
 be_valid_cpu_generator_core_config = _be_valid_cpu_generator_core_config()
 be_valid_cpu_generator_core_config_targets = _be_valid_cpu_generator_core_config_targets()
 be_valid_cpu_generator_result = _be_valid_cpu_generator_result()
+be_valid_cpu_generator_stats = _be_valid_cpu_generator_stats()
+be_valid_cpu_generator_core_stats = _be_valid_cpu_generator_core_stats()
