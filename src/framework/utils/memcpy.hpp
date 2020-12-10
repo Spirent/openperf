ed842f52125dde8bfb0fe1a8fca69037e0250db5
Timmons C. Player <timmons.player@spirent.com>
2020-12-10 22:40:01 +0000

    framework/utils: update memcpy
    Update the memcpy functions to just call std::memcpy if no optimized
    version is available.

--- src/framework/utils/memcpy.hpp
+++ src/framework/utils/memcpy.hpp
@@ -4,30 +4,42 @@
 #include <cstdint>
 #include <cstring>
 
-#include "utils/dpdk/memcpy_avx512.hpp"
-#include "utils/dpdk/memcpy_avx2.hpp"
-#include "utils/dpdk/memcpy_sse.hpp"
-
 /* Turn compiler generated instruction set macros into constexpr booleans */
 #if defined(__AVX512F__)
 static constexpr bool avx512_enabled = true;
+#include "utils/dpdk/memcpy_avx512.hpp"
 #else
 static constexpr bool avx512_enabled = false;
+namespace dpdk::impl::avx512 {
+inline constexpr int alignment_mask = 0;
+void* rte_memcpy_aligned(void*, const void*, size_t);
+void* rte_memcpy_generic(void*, const void*, size_t);
+} // namespace dpdk::impl::avx512
 #endif
 
 #if defined(__AVX2__) && !defined(__AVX512F__)
 static constexpr bool avx2_enabled = true;
+#include "utils/dpdk/memcpy_avx2.hpp"
 #else
 static constexpr bool avx2_enabled = false;
+namespace dpdk::impl::avx2 {
+inline constexpr int alignment_mask = 0;
+void* rte_memcpy_aligned(void*, const void*, size_t);
+void* rte_memcpy_generic(void*, const void*, size_t);
+} // namespace dpdk::impl::avx2
 #endif
 
 #if defined(__SSSE3__) && !defined(__AVX2__)
 static constexpr bool sse_enabled = true;
+#include "utils/dpdk/memcpy_sse.hpp"
 #else
 static constexpr bool sse_enabled = false;
+namespace dpdk::impl::sse {
+inline constexpr int alignment_mask = 0;
+void* rte_memcpy_aligned(void*, const void*, size_t);
+void* rte_memcpy_generic(void*, const void*, size_t);
+} // namespace dpdk::impl::sse
 #endif
-
-static_assert(avx512_enabled || avx2_enabled || sse_enabled);
 
 namespace openperf::utils {
 
@@ -38,6 +50,8 @@ constexpr int memcpy_alignment_mask()
     }
     if constexpr (avx2_enabled) { return (dpdk::impl::avx2::alignment_mask); }
     if constexpr (sse_enabled) { return (dpdk::impl::sse::alignment_mask); }
+
+    return (0x7);
 }
 
 constexpr int memcpy_alignment() { return (memcpy_alignment_mask() + 1); }
@@ -71,6 +85,8 @@ inline void* memcpy(void* dst, const void* src, size_t n)
             return dpdk::impl::sse::rte_memcpy_generic(dst, src, n);
         }
     }
+
+    return std::memcpy(dst, src, n);
 }
 
 } // namespace openperf::utils
