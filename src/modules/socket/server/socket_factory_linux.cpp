101df0da54cbb0775c4f23ef948017fab31dfd8d
Charles Myers <charles.myers@spirent.com>
2021-01-21 21:12:36 -1000

    modules/socket: catch exception when eventd create fails
    catch exception thrown when eventfd create fails and
    convert to tl::expected so api can return a valid response.

--- src/modules/socket/server/socket_factory_linux.cpp
+++ src/modules/socket/server/socket_factory_linux.cpp
@@ -1,4 +1,5 @@
 #include <cerrno>
+#include <system_error>
 
 #include "socket/server/generic_socket.hpp"
 #include "socket/server/packet_socket.hpp"
@@ -12,17 +13,22 @@ make_socket(openperf::socket::server::allocator& allocator,
             int type,
             int protocol)
 {
-    if (domain == AF_PACKET) {
-        switch (type & 0xff) {
-        case SOCK_DGRAM:
-        case SOCK_RAW:
-            return (generic_socket(packet_socket(allocator, type, protocol)));
-        default:
-            return (tl::make_unexpected(EACCES));
+    try {
+        if (domain == AF_PACKET) {
+            switch (type & 0xff) {
+            case SOCK_DGRAM:
+            case SOCK_RAW:
+                return (
+                    generic_socket(packet_socket(allocator, type, protocol)));
+            default:
+                return (tl::make_unexpected(EACCES));
+            }
         }
-    }
 
-    return (make_socket_common(allocator, domain, type, protocol));
+        return (make_socket_common(allocator, domain, type, protocol));
+    } catch (const std::system_error& e) {
+        return (tl::make_unexpected(e.code().value()));
+    }
 }
 
 } // namespace openperf::socket::server
