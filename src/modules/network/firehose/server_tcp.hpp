0d1579b45f4a979eb643da21b37e946909406982
Charles Myers <charles.myers@spirent.com>
2021-01-27 14:19:25 -1000

    modules/network: add tcp_acceptor class
    Added tcp_acceptor class to split out the accept code from the
    tcp_server class.
    
    Fixed issue accessing socket fd from multiple threads.
    This is not thread safe for DPDK sockets and could cause
    issues with kernel sockets too.

--- src/modules/network/firehose/server_tcp.hpp
+++ src/modules/network/firehose/server_tcp.hpp
@@ -7,8 +7,34 @@
 #include <zmq.h>
 
 #include "server.hpp"
+#include "core/op_socket.h"
+
+namespace openperf::core {
+class event_loop;
+}
 
 namespace openperf::network::internal::firehose {
+
+class tcp_acceptor
+{
+private:
+    drivers::driver_ptr m_driver;
+    std::unique_ptr<void, op_socket_deleter> m_accept_pub;
+    std::thread m_thread;
+    int m_eventfd;
+    int m_acceptfd;
+    bool m_running;
+
+    void run();
+
+public:
+    tcp_acceptor(const drivers::driver_ptr& driver, void* context);
+    ~tcp_acceptor();
+
+    void start(int acceptfd);
+    void stop();
+    bool running() const { return m_running; }
+};
 
 class server_tcp final : public server
 {
@@ -23,15 +49,11 @@ private:
     };
 
     std::atomic_bool m_stopped;
-    std::thread m_accept_thread;
     std::vector<std::unique_ptr<std::thread>> m_worker_threads;
     std::unique_ptr<void, zmq_ctx_deleter> m_context;
+    tcp_acceptor m_acceptor;
 
     int tcp_write(connection_t&, std::vector<uint8_t> send_buffer);
-    tl::expected<int, std::string>
-    new_server(int domain,
-               in_port_t port,
-               const std::optional<std::string>& interface);
     void run_accept_thread();
     void run_worker_thread();
 
