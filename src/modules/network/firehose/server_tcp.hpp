3c5c9aa9cd6e1f45ef2d24552192ba333603406e
Charles Myers <charles.myers@spirent.com>
2021-02-05 16:32:54 -1000

    modules/network: add tcp_worker class and use event_loop
    This change extracts the tcp_server worker thread to it's own class.
    This cleans up the code and makes it easier to support multiple
    worker threads each using the event_loop class for dispatching
    sockets events.

--- src/modules/network/firehose/server_tcp.hpp
+++ src/modules/network/firehose/server_tcp.hpp
@@ -4,6 +4,7 @@
 #include <atomic>
 #include <thread>
 #include <vector>
+#include <map>
 #include <zmq.h>
 
 #include "server.hpp"
@@ -14,6 +15,18 @@ class event_loop;
 }
 
 namespace openperf::network::internal::firehose {
+
+class tcp_worker;
+
+struct tcp_connection_t
+{
+    int fd;
+    connection_state_t state;
+    size_t bytes_left;
+    std::vector<uint8_t> request;
+    network_sockaddr client;
+    tcp_worker* worker;
+};
 
 class tcp_acceptor
 {
@@ -29,10 +42,54 @@ private:
 
 public:
     tcp_acceptor(const drivers::driver_ptr& driver, void* context);
+    tcp_acceptor(const tcp_acceptor&) = delete;
+    tcp_acceptor(tcp_acceptor&&) = delete;
+    tcp_acceptor& operator=(const tcp_acceptor&) = delete;
+    tcp_acceptor& operator=(tcp_acceptor&&) = delete;
     ~tcp_acceptor();
 
     void start(int acceptfd);
     void stop();
+    bool running() const { return m_running; }
+};
+
+class tcp_worker
+{
+private:
+    friend class tcp_connection_reading_state;
+    friend class tcp_connection_writing_state;
+
+    drivers::driver_ptr m_driver;
+    server::stat_t& m_stat;
+    std::unique_ptr<core::event_loop> m_loop;
+    std::unique_ptr<void, op_socket_deleter> m_ctrl_sub;
+    std::unique_ptr<void, op_socket_deleter> m_accept_sub;
+    std::thread m_thread;
+    bool m_running;
+    std::map<int, std::unique_ptr<tcp_connection_t>> m_connections;
+    std::vector<uint8_t> m_read_buffer;
+    std::vector<uint8_t> m_write_buffer;
+
+    void run();
+
+    int do_accept();
+    int do_control();
+    int do_close(tcp_connection_t& conn);
+    int do_read(tcp_connection_t& conn);
+    int do_write(tcp_connection_t& conn);
+
+public:
+    tcp_worker(const drivers::driver_ptr& driver,
+               void* context,
+               server::stat_t& stat);
+    tcp_worker(const tcp_worker&) = delete;
+    tcp_worker(tcp_worker&&) = delete;
+    tcp_worker& operator=(const tcp_worker&) = delete;
+    tcp_worker& operator=(tcp_worker&&) = delete;
+    ~tcp_worker();
+
+    void start();
+    void join();
     bool running() const { return m_running; }
 };
 
@@ -48,14 +105,13 @@ private:
         }
     };
 
-    std::atomic_bool m_stopped;
-    std::vector<std::unique_ptr<std::thread>> m_worker_threads;
     std::unique_ptr<void, zmq_ctx_deleter> m_context;
-    tcp_acceptor m_acceptor;
+    std::unique_ptr<void, op_socket_deleter> m_ctrl_pub;
+    std::unique_ptr<tcp_acceptor> m_acceptor;
+    std::vector<std::unique_ptr<tcp_worker>> m_workers;
 
-    int tcp_write(connection_t&, std::vector<uint8_t> send_buffer);
-    void run_accept_thread();
-    void run_worker_thread();
+    void start_workers();
+    void stop_workers();
 
 public:
     server_tcp(in_port_t port,
