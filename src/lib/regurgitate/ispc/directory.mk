9755fb83f97acc4986f5bc4f08878c20f8635928
Timmons C. Player <timmons.player@spirent.com>
2021-05-14 21:38:05 +0000

    lib/regurgitate: add missing ISPC/aarch64 symbol
    ISPC is apparently missing a uniform double `ceil` implementation. Give
    it one.

--- src/lib/regurgitate/ispc/directory.mk
+++ src/lib/regurgitate/ispc/directory.mk
@@ -3,3 +3,9 @@ REGURGITATE_SOURCES += \
 	ispc/merge_float_float.ispc \
 	ispc/sort_float_double.ispc \
 	ispc/sort_float_float.ispc
+
+# XXX: ISPC seems to be missing __ceil_uniform_double for Aarch64, so add
+# the proper symbol for it.
+ifeq ($(ARCH),aarch64)
+	REGURGITATE_SOURCES += ispc/ceil_uniform_double.c
+endif
