28870a994a61b8ec93afa7f8d01334aca912126e
Daniel Morton <daniel.morton@spirent.com>
2021-06-02 15:19:26 -1000

    Minor fix for building as a DPDK secondary process

--- src/modules/packetio/drivers/dpdk/secondary/driver_factory.cpp
+++ src/modules/packetio/drivers/dpdk/secondary/driver_factory.cpp
@@ -1,6 +1,6 @@
 #include "packetio/generic_driver.hpp"
 #include "packetio/drivers/dpdk/driver.tcc"
-#include "packetio/drivers/dummy_driver.hpp"
+#include "packetio/drivers/dpdk/dummy_driver.hpp"
 #include "packetio/drivers/dpdk/secondary/eal_process.hpp"
 
 namespace openperf::packetio {
@@ -11,7 +11,7 @@ namespace openperf::packetio::driver {
 
 std::unique_ptr<generic_driver> make()
 {
-    if (config::dpdk::dpdk_disabled()) {
+    if (dpdk::config::dpdk_disabled()) {
         return (std::make_unique<generic_driver>(dpdk::dummy_driver()));
     }
 
