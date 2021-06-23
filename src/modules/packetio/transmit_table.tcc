Spirent/openperf

commit 5918c74307a7e1dac961e346034dffcde8847904

modules/packetio: fix use after free bug
The transmit scheduler kept a pointer to a string which could get
deleted in between run states. Fix the issue by using a safe key type
which contains only primitive types.

src/modules/packetio/transmit_table.tcc
@@ -21,6 +21,16 @@ typename transmit_table<Source>::key_type transmit_table<Source>::to_key(
     return {port_idx, queue_idx, std::string(source_id)};
 }
 
+template <typename Source>
+typename transmit_table<Source>::safe_key_type
+transmit_table<Source>::to_safe_key(
+    const typename transmit_table<Source>::key_type& key)
+{
+    return {std::get<key_port_idx>(key),
+            std::get<key_queue_idx>(key),
+            std::hash<std::string>{}(std::get<key_id_idx>(key))};
+}
+
 /*
  * The various binary search algorithms, e.g. equal_range, lower_bound, etc.,
  * need to make the "less than" comparison between keys and values in any order.
@@ -42,20 +52,18 @@ template <typename Source> struct key_comparator
     }
 };
 
-template <typename Source> struct noalloc_key_comparator
+template <typename Source> struct safe_key_comparator
 {
-    bool
-    operator()(const typename transmit_table<Source>::noalloc_key_type& left,
-               const typename transmit_table<Source>::value_type& right)
+    bool operator()(const typename transmit_table<Source>::safe_key_type& left,
+                    const typename transmit_table<Source>::value_type& right)
     {
-        return (left < right.first);
+        return (left < transmit_table<Source>::to_safe_key(right.first));
     }
 
-    bool
-    operator()(const typename transmit_table<Source>::value_type& left,
-               const typename transmit_table<Source>::noalloc_key_type& right)
+    bool operator()(const typename transmit_table<Source>::value_type& left,
+                    const typename transmit_table<Source>::safe_key_type& right)
     {
-        return (left.first < right);
+        return (transmit_table<Source>::to_safe_key(left.first) < right);
     }
 };
 
@@ -171,11 +179,11 @@ const Source* transmit_table<Source>::get_source(
 
 template <typename Source>
 const Source* transmit_table<Source>::get_source(
-    const transmit_table<Source>::noalloc_key_type& key) const
+    const transmit_table<Source>::safe_key_type& key) const
 {
     auto map = m_sources.load(std::memory_order_consume);
     auto range = std::equal_range(
-        map->begin(), map->end(), key, noalloc_key_comparator<Source>{});
+        map->begin(), map->end(), key, safe_key_comparator<Source>{});
     assert(std::distance(range.first, range.second) <= 1);
     return (std::distance(range.first, range.second) == 1
                 ? std::addressof(range.first->get().second)

src/modules/packetio/transmit_table.hpp
@@ -22,7 +22,7 @@ public:
     static constexpr auto key_id_idx = 2;
 
     using key_type = std::tuple<uint16_t, uint16_t, std::string>;
-    using noalloc_key_type = std::tuple<uint16_t, uint16_t, std::string_view>;
+    using safe_key_type = std::tuple<uint16_t, uint16_t, size_t>;
     using mapped_type = Source;
     using value_type = std::pair<key_type, mapped_type>;
     using source_entry = immer::box<value_type>;
@@ -41,7 +41,9 @@ public:
     get_sources(uint16_t port_idx, uint16_t queue_idx) const;
 
     const Source* get_source(const key_type&) const;
-    const Source* get_source(const noalloc_key_type&) const;
+    const Source* get_source(const safe_key_type&) const;
+
+    static safe_key_type to_safe_key(const key_type&);
 
 private:
     std::atomic<source_map*> m_sources;

src/modules/packetio/workers/dpdk/tx_scheduler.cpp
@@ -151,7 +151,7 @@ void tx_scheduler::do_reschedule(const schedule::time_point& now)
     const auto& priority_vec = get_container(m_schedule);
 
     for (const auto& key_source : m_tib.get_sources(port_id(), queue_id())) {
-        const auto& key = key_source->first;
+        const auto key = worker::tib::to_safe_key(key_source->first);
         const auto& source = key_source->second;
         if (source.active()
             && std::none_of(
@@ -460,10 +460,10 @@ void tx_scheduler::on_transition(const schedule::state_running& state)
     if (m_schedule.empty()) {
         /* Generate a schedule for all available entities */
         for (auto&& pair : m_tib.get_sources(port_id(), queue_id())) {
-            const auto& key = pair->first;
             const auto& source = pair->second;
             if (source.active()) {
-                m_schedule.push({now + next_deadline(source), key});
+                m_schedule.push({now + next_deadline(source),
+                                 worker::tib::to_safe_key(pair->first)});
             }
         }
 

src/modules/packetio/workers/dpdk/tx_scheduler.hpp
@@ -20,7 +20,7 @@ using time_point = std::chrono::time_point<clock>;
 struct entry
 {
     time_point deadline;
-    worker::tib::noalloc_key_type key;
+    worker::tib::safe_key_type key;
 };
 
 constexpr bool operator>(const entry& left, const entry& right);

commit 6856937d9dac8661f786f3420ef1de84443b74b1

modules/packetio: fix transmit table ordering issue
Oops. Fix bug introduced in 5918c74307a7e1dac961e346034dffcde8847904.
Obviously, hashes of strings won't be ordered the same way the strings
will, so fix the function that searched by the hashed string value.

src/modules/packetio/transmit_table.tcc
@@ -52,52 +52,48 @@ template <typename Source> struct key_comparator
     }
 };
 
+template <typename Source, typename T>
+static inline std::pair<uint16_t, uint16_t>
+to_port_queue_pair(const std::tuple<uint16_t, uint16_t, T>& key)
+{
+    static constexpr auto key_port_idx = transmit_table<Source>::key_port_idx;
+    static constexpr auto key_queue_idx = transmit_table<Source>::key_queue_idx;
+
+    return (std::make_pair(std::get<key_port_idx>(key),
+                           std::get<key_queue_idx>(key)));
+}
+
 template <typename Source> struct safe_key_comparator
 {
     bool operator()(const typename transmit_table<Source>::safe_key_type& left,
                     const typename transmit_table<Source>::value_type& right)
     {
-        return (left < transmit_table<Source>::to_safe_key(right.first));
+        return (to_port_queue_pair<Source>(left)
+                < to_port_queue_pair<Source>(right.first));
     }
 
     bool operator()(const typename transmit_table<Source>::value_type& left,
                     const typename transmit_table<Source>::safe_key_type& right)
     {
-        return (transmit_table<Source>::to_safe_key(left.first) < right);
+        return (to_port_queue_pair<Source>(left.first)
+                < to_port_queue_pair<Source>(right));
     }
 };
 
 template <typename Source> struct port_queue_comparator
 {
-    static constexpr auto key_port_idx = transmit_table<Source>::key_port_idx;
-    static constexpr auto key_queue_idx = transmit_table<Source>::key_queue_idx;
-
     bool operator()(const typename transmit_table<Source>::key_type& left,
                     const typename transmit_table<Source>::value_type& right)
     {
-        /* Turn port/queue indexes into a number and compare them */
-        auto left_id =
-            (static_cast<uint32_t>(std::get<key_port_idx>(left)) << 16
-             | std::get<key_queue_idx>(left));
-        auto right_id =
-            (static_cast<uint32_t>(std::get<key_port_idx>(right.first)) << 16
-             | std::get<key_queue_idx>(right.first));
-
-        return (left_id < right_id);
+        return (to_port_queue_pair<Source>(left)
+                < to_port_queue_pair<Source>(right.first));
     }
 
     bool operator()(const typename transmit_table<Source>::value_type& left,
                     const typename transmit_table<Source>::key_type& right)
     {
-        /* Turn port/queue indexes into a number and compare them */
-        auto left_id =
-            (static_cast<uint32_t>(std::get<key_port_idx>(left.first)) << 16
-             | std::get<key_queue_idx>(left.first));
-        auto right_id =
-            (static_cast<uint32_t>(std::get<key_port_idx>(right)) << 16
-             | std::get<key_queue_idx>(right));
-
-        return (left_id < right_id);
+        return (to_port_queue_pair<Source>(left.first)
+                < to_port_queue_pair<Source>(right));
     }
 };
 
@@ -182,12 +178,20 @@ const Source* transmit_table<Source>::get_source(
     const transmit_table<Source>::safe_key_type& key) const
 {
     auto map = m_sources.load(std::memory_order_consume);
+    /*
+     * XXX: safe_key_comparator only compares port/queue indexes.
+     * We still need to explicitly find the matching hash value.
+     */
     auto range = std::equal_range(
         map->begin(), map->end(), key, safe_key_comparator<Source>{});
-    assert(std::distance(range.first, range.second) <= 1);
-    return (std::distance(range.first, range.second) == 1
-                ? std::addressof(range.first->get().second)
-                : nullptr);
+    const auto hash = std::get<key_id_idx>(key);
+    auto cursor =
+        std::find_if(range.first, range.second, [&](const auto& item) {
+            return (std::hash<std::string>{}(std::get<key_id_idx>(item->first))
+                    == hash);
+        });
+    return (cursor == range.second ? nullptr
+                                   : std::addressof(cursor->get().second));
 }
 
 } // namespace openperf::packetio