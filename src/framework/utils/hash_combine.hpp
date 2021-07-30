d67be1b8f1ebaf9a695358d9e00905841f0df996
Timmons C. Player <timmons.player@spirent.com>
2021-07-30 22:07:33 +0000

    framework: fix hash combine function

--- src/framework/utils/hash_combine.hpp
+++ src/framework/utils/hash_combine.hpp
@@ -13,7 +13,7 @@ namespace openperf::utils {
 template <typename T> size_t hash_combine(size_t seed, const T& value)
 {
     std::hash<T> hasher;
-    return (seed ^ hasher(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2));
+    return (seed ^ (hasher(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2)));
 }
 
 /**
