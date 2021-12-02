6e6142351b63d6a3983668d76265ecea5e9d2344
Timmons C. Player <timmons.player@spirent.com>
2021-12-02 20:29:26 +0000

    modules/block: use cpuset based masks
    Update the block module to use the framework cpuset for specifying core
    masks.

--- src/modules/block/block_options.c
+++ src/modules/block/block_options.c
@@ -1,11 +1,13 @@
 #include "core/op_core.h"
 #include "config/op_config_file.hpp"
 
+const char op_block_mask[] = "modules.block.cpu-mask";
+
 MAKE_OPTION_DATA(
     block,
     NULL,
     MAKE_OPT("specifies CPU core mask for all Block module threads, in hex",
-             "modules.block.cpu-mask",
+             op_block_mask,
              0,
              OP_OPTION_TYPE_HEX), );
 
